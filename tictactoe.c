dac6755fac112dbc3d30883c44beef4bccf1f11c
Maria Luque Anguita <mluqueanguita@gmail.com>
2019-03-15 12:59:58 +0000

    cleaned a bit

--- tictactoe.c
+++ tictactoe.c
@@ -16,7 +16,6 @@ int winner();
 int check_end();
 int check_movement(int, char);
 void init_stack(struct stack *);
-void display(struct stack *);
 void push(struct stack*, int);
 int *pop(struct stack *);
 void reverse_string(char *str);
@@ -43,7 +42,7 @@ int main()
 
   int computer_game; // multiplayer or against the computer
   printf("\n\n ***********************\n *     TIC TAC TOE     *\n *                     *\n *      by Maria       *\n *      SET08122       *\n ***********************\n");
-  printf("\nChoose number of players:\n\n- 1 to play agains the computer\n- 2 players 1v1\n- 3 to input an existing game and continue (file must be called test.txt and be in the same folder as the game :) )\n\nType 1, 2 or 3:\n\n");
+  printf("\nChoose number of players:\n\n- 1 to play agains the computer\n- 2 players 1v1\n- 3 to input an existing game and continue playing it\n\nType 1, 2 or 3:\n\n");
   scanf("%d", &computer_game);
 
   if (computer_game == 1 || computer_game == 2)
@@ -56,67 +55,44 @@ int main()
     printf("\n Enter Player 2's name: ");
     scanf("%s", name2);
   }
-  else if (computer_game == 3){
-
-    file = fopen("test.txt", "r");
+  else if (computer_game == 3) // Read game from file
+  {
+    file = fopen("game.txt", "r");
     fgets(name1, 20, (FILE*)file);    // read first line: name1 (Player 1)
     fgets(buffer, 15, (FILE*)file);   // read first stack (original movements)
-    //printf("buffer: %s\n", buffer);
     reverse_string(buffer);
-    //printf("after reverse: %s\n", buffer);
-    //printf("strlen(buffer): %d\n", strlen(buffer));
 
-    player = player % 2;
-    printf("player outside: %d\n", player);
-
-    // FIRST STACK
-    // go through every number in buffer and add to first stack
+    // FIRST STACK go through every number in buffer and add to first stack
     for (int i = 1; i < strlen(buffer); i++)
     {
       push(&first, (int)(buffer[i] - '0'));
-      //printf("pushed to first: %d\n", (int)(buffer[i] - '0'));
-      player = player % 2;
-      printf("player inside: %d\n", player);
-      if (player == 1){
-        printf("number for player 1 X: %d\n", numbers[((int)(buffer[i] - '0')-1)]);
+      player = player % 2; // alternate player to set Xs and Os
+      if (player == 1)
         numbers[((int)(buffer[i] - '0')-1)] = 'X';
-      }
-      else if (player == 0){
-        printf("number for player 2 O: %d\n", numbers[((int)(buffer[i] - '0')-1)]);
+      else if (player == 0)
         numbers[((int)(buffer[i] - '0')-1)] = 'O';
-      }
-
       player++;
     }
 
     fgets(name2, 20, (FILE*)file);    // read 3rd line: name2 (Player 2)
     fgets(buffer, 15, (FILE*)file);   // read second stack (undo movements)
-    //printf("buffer: %s\n", buffer);
     reverse_string(buffer);
-    //printf("after reverse: %s\n", buffer);
-    //printf("strlen(buffer): %d\n", strlen(buffer));
 
-    // SECOND STACK
-    // go through every number in buffer and add to second stack
+    // SECOND STACK go through every number in buffer and add to second stack
     for (int i = 1; i < strlen(buffer); i++)
-    {
       push(&second, (int)(buffer[i] - '0'));
-      //printf("pushed to second: %d\n", (int)(buffer[i] - '0'));
-    }
-
     fclose(file);
   }
 
   do
   {
     print_grid(name1, name2);
-
     player = player % 2;
 
     if (player == 1) // print name and options
     {
       player = 1;
-      printf(" %s choose a number for your next move / 10 for undo / 20 for redo / 30 to save game to a file: \n", name1);
+      printf("%s choose a number for your next move / 10 for undo / 20 for redo / 30 to save game to a file: \n", name1);
       scanf("%d", &choice);
     }
     else
@@ -148,59 +124,34 @@ int main()
     /*
       OPTIONS:
         - play a valid move
-        - the move is invalid
-        - undo
-        - redo
+        - invalid move
+        - 10: undo
+        - 20: redo
+        - 30: export game to a file
     */
     if (choice == 10) // UNDO - pop item from first and push into second
     {
       move = pop(&first); // pop from first
       if (move)
       {
-        push(&second, *move); // push into second
-
-        // set grid as top in first - pop second, change first X or O for first number, push it back to second
-        move = pop(&second);
-
-        if (move)
-        {
-          // from X or O to 3
-          numbers[*move-1] = *move + '0';
-          print_grid(name1, name2);
-          push(&second, *move); // push it back to second
-        }
+        numbers[*move-1] = *move + '0'; // from X or O to number
+        push(&second, *move); // push to second
+        print_grid(name1, name2);
       }
     }
     else if (choice == 20) // REDO
     {
-      move = pop(&second); // pop from first
+      move = pop(&second); // pop from second
       if (move)
       {
-        push(&first, *move); // push into second
-
-        // set grid as top in first - pop second, change first X or O for first number, push it back to second
-        move = pop(&first);
-
-        if (move)
-        {
-          printf("Last move: %d\n", *move);
-
-          // get first digit of last_move to find what space to change
-          //while(*move >= 10)
-            //*move = *move / 10;
-
-          // from 3 to X or O
-          numbers[*move-1] = type;
-          print_grid(name1, name2);
-
-          push(&first, *move); // push it back to first
-        }
+        numbers[*move-1] = type; // from number to X or O
+        push(&first, *move);
+        print_grid(name1, name2);
       }
     }
     else if (choice == 30) // EXPORT GAME TO FILE
     {
-      file = fopen("test.txt", "w+");
-
+      file = fopen("game.txt", "w+");
       fprintf(file, "%s\n", name1);
 
       for(int i=first.top; i>=0; --i)
@@ -212,7 +163,6 @@ int main()
         fprintf(file, "%d", second.array[i]);
 
       fprintf(file, "\nEOF");
-
       fclose(file);
     }
     // INVALID MOVEMENT
@@ -224,22 +174,15 @@ int main()
     }
     else // VALID MOVEMENT - add movement to history of moves
     {
-      /*
-         51 means 5 for choice and 1 for X
-         1 for X (player 1)
-         2 for O (player 2)
-         3 for - not played yet
-      */
       if ((choice != 10)&&(choice != 20))
       {
         int moves_number = choice;
         push(&first, moves_number); // push to first - add move to stack
       }
     }
-
     result = winner();
     player++;
-  }while (result == 3);
+  }while (result == 3); // while still playing
 
   print_grid(name1, name2);
 
@@ -261,7 +204,7 @@ int main()
 
 /*
   check if movement is valid
-  return 0 for invalid, 1 for valid, 2 for undo, 3 for redo
+  return 0 for invalid, 1 for valid
 */
 int check_movement(int choice, char type)
 {
@@ -293,7 +236,7 @@ int check_movement(int choice, char type)
 
 void print_grid(char *name1, char *name2)
 {
-  //system("cls");
+  system("cls");
 
   printf("\n\n\n              |     |     \n");
   printf("           %c  |  %c  |  %c \n", numbers[0], numbers[1], numbers[2]);
@@ -305,10 +248,6 @@ void print_grid(char *name1, char *name2)
 
 int winner()
 {
-  // 1 show winner
-  // 2 no winner
-  // 3 still playing
-
   if ((numbers[0] == numbers[1] && numbers[1] == numbers[2])      //VERTICAL 1
         || (numbers[3] == numbers[4] && numbers[4] == numbers[5]) //VERTICAL 2
         || (numbers[6] == numbers[7] && numbers[7] == numbers[8]) //VERTICAL 3
@@ -334,24 +273,7 @@ int check_end()
     if ((numbers[i] != 'X') && (numbers[i] != 'O'))
       return 0;
   }
-  return 1;
-}
-
-void display(struct stack *s)
-{
-	int i;
-	if(s->top==-1)
-		printf("Stack is empty\n");
-	else
-	{
-		printf("\nStack is...\n");
-		for(i=s->top; i>=0; --i)
-    {
-      printf("s->array[i]: %d\n", s->array[i]);
-      //printf("concatenate(moves, s->array[i]): %d\n", concatenate(moves, s->array[i]));
-      //moves = concatenate(moves, s->array[i]);
-    }
-	}
+  return 1; // no winner
 }
 
 /*
